470d2d5bfbce4862d7331a761eb2176ef0e3ec2f
Rakshit Choudhary <rakshitchoudhary8@gmail.com>
2019-11-19 00:01:25 +0530

    new

--- main.py
+++ main.py
@@ -9,31 +9,41 @@ string = 'Apr 29 06:56:48 example-server sshd[38254]: pam_unix(sshd:auth): authe
 string = 'Apr 30 23:50:10 example-server sshd[125129]: Failed password for root from 122.226.181.166 port 35148 ssh2'
 string = 'Apr 30 23:51:26 example-server sshd[125321]: Failed password for invalid user sebastian from 129.213.16.142 port 49420 ssh2'
 
-
-def temp_function():
-    date = string[0:6]
-    if string.find("Failed password ")>=0 and string.find("root ")>=0:
-        print('1',re.split("root from ", string)[1].split('port')[0])
-    else :
-        print('2',re.split(" from ", string)[1].split('port')[0])
-    print(date)
+dict = {}
+# def temp_function():
+#
+#     date = string[0:6]
+#     if string.find("Failed password ")>=0 and string.find("root ")>=0:
+#         print('1',re.split("root from ", string)[1].split('port')[0])
+#     else :
+#         print('2',re.split(" from ", string)[1].split('port')[0])
+#
+#     if date not in dict.keys():
+#         dict[date] = 300
+#
+#     print(date)
 
 def logger_function():
     with open(log_file_path, "r") as file:
-        match_list = []
         if read_line == True:
             for line in file:
                 print(line)
-                for match in re.finditer(regex, line, re.S):
-                    match_text = match.group()
-                    match_list.append(match_text)
-                    print
-                    match_text
-        else:
-            data = f.read()
-            for match in re.finditer(regex, data, re.S):
-                match_text = match.group()
-                match_list.append(match_text)
+                string=line
+                date = string[0:6]
+                if string.find("Failed password ")>=0 and string.find("root ")>=0:
+                    print('1',re.split("root from ", string)[1].split('port')[0])
+                else :
+                    print('2',re.split(" from ", string)[1].split('port')[0])
+
+                if date not in dict.keys():
+                    dict[date] = 300
+
+                print(date)
+        # else:
+        #     data = f.read()
+        #     for match in re.finditer(regex, data, re.S):
+        #         match_text = match.group()
+        #         match_list.append(match_text)
     file.close()
 
 def myfun():
@@ -41,6 +51,6 @@ def myfun():
 
 
 myfun()
-temp_function()
+logger_function()
 
 
